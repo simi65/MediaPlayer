8356f41d3d2be750a2036dfdc73c240404f242e5
MISBAH <misbah@MacBook-Pro-1.local>
2019-05-09 21:48:31 +0700

    Initial commit

---
+++ app/src/main/java/com/miss/mediaplayer/MainActivity.java
@@ -0,0 +1,117 @@
+package com.miss.mediaplayer;
+
+import android.media.MediaPlayer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import java.io.IOException;
+
+public class MainActivity extends AppCompatActivity {
+    /** Called when the activity is first created. */
+
+    private Button btnPlay;
+    private Button btnPause;
+    private Button btnStop;
+    private MediaPlayer mp;
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mp = new MediaPlayer();
+
+        btnPlay = (Button) findViewById(R.id.btnPLAY);
+        btnPause = (Button) findViewById(R.id.btnPAUSE);
+        btnStop = (Button) findViewById(R.id.btnSTOP);
+
+        stateAwal();
+
+        btnPlay.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                play();
+                btnPlay.setEnabled(false);
+                btnPause.setEnabled(true);
+                btnStop.setEnabled(true);
+            }
+        });
+
+        btnPause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                pause();
+            }
+        });
+
+        btnStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                stop();
+            }
+        });
+    }
+
+    /** State Awal / Pertama Dijalankan */
+    public void stateAwal(){
+        btnPlay.setEnabled(true);
+        btnPause.setEnabled(false);
+        btnStop.setEnabled(false);
+    }
+
+    /** Dijalankan Oleh Tombol Play */
+    private void play() {
+        /** Memanggil File MP3 "indonesiaraya.mp3" */
+        mp = MediaPlayer.create(this, R.raw.adzan_muhammad_thaha);
+
+        try {
+            mp.prepare();
+        } catch (IllegalStateException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        /** Menjalankan Audio */
+        mp.start();
+
+        /** Penanganan Ketika Suara Berakhir */
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                stateAwal();
+            }
+        });
+    }
+
+    /** Dijalankan Oleh Tombol Pause */
+    public void pause(){
+        if(mp.isPlaying()){
+            if(mp!=null){
+                mp.pause();
+
+            }
+        } else {
+            if(mp!=null){
+                mp.start();
+
+            }
+        }
+    }
+
+    /** Dijalankan Oleh Tombol Stop */
+    public void stop(){
+        mp.stop();
+
+        try{
+            mp.prepare();
+            mp.seekTo(0);
+        }catch (Throwable t) {
+            t.printStackTrace();
+        }
+
+        stateAwal();
+    }
+}
